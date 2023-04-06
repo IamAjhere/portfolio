9d7c8a1f7e7ef79eb067098241435e101e4fdb27
IamAjhere <Ajcreationsajmal@gmail.com>
2023-04-06 21:20:55 +0530

    feature/git-api and about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import CustomCursor from "./components/Cursor/CustomCursor";
 import GalaxyBackground from "./components/GalaxyBackground";
@@ -6,6 +7,7 @@ import About from "./components/Sections/About";
 import Contact from "./components/Sections/Contact";
 import Projects from "./components/Sections/Projects";
 import Skills from "./components/Sections/Skills";
+import axios from "axios";
 
 const navLinks = [
   { label: "About", href: "about" },
@@ -13,25 +15,54 @@ const navLinks = [
   { label: "Projects", href: "projects" },
   { label: "Contact", href: "contact" },
 ];
-
 function App() {
+  const [profile, setProfile] = useState<IProfile | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.github.com/users/IamAjHere"
+        );
+        setProfile(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log(profile);
   return (
     <div className={`relative w-full h-full`}>
       <CustomCursor />
-      <Header name="Mohamed Ajmal" navLinks={navLinks} />
+      <Header name={`${profile?.name}`} navLinks={navLinks} />
       <GalaxyBackground />
       <div className="absolute top-0 left-0 w-full h-full">
-        <div id="about" className="h-screen">
-          <About />
-        </div>
-        <div id="skills" className="h-screen">
-          <Skills />
-        </div>
-        <div id="projects" className="h-screen">
-          <Projects />
-        </div>
-        <div id="contact" className="h-screen">
-          <Contact />
+        <div className="container mx-auto px-4">
+          <div
+            id="about"
+            className="h-screen border-l-2 border-r-2 border-white"
+          >
+            <About profile={profile} />
+          </div>
+          <div
+            id="skills"
+            className="h-screen border-l-2 border-r-2 border-white"
+          >
+            <Skills />
+          </div>
+          <div
+            id="projects"
+            className="h-screen border-l-2 border-r-2 border-white"
+          >
+            <Projects />
+          </div>
+          <div
+            id="contact"
+            className="h-screen border-l-2 border-r-2 border-white"
+          >
+            <Contact />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -1,13 +1,33 @@
 import React from "react";
 
-function About() {
+interface AboutProps {
+  profile: IProfile;
+}
+
+const About: React.FC<AboutProps> = ({ profile }) => {
   return (
-    <div className="w-full h-full flex items-center justify-center bg-transparent">
-      <h1 className="text-6xl font-bold text-center text-white">
-        Hi, I am Ajmal
+    <div className="w-full h-full flex flex-col items-center justify-center bg-transparent">
+      <div className="rounded-full overflow-hidden w-32 h-32 mb-8">
+        <img
+          src={profile?.avatar_url}
+          alt={profile?.name}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <h1 className="text-4xl sm:text-6xl font-bold text-center text-white mb-4">
+        Hi, I am {profile?.name}
       </h1>
+      <p className="text-base sm:text-xl text-center text-white mx-8 mb-4">
+        {profile?.bio}
+      </p>
+      <p className="text-sm sm:text-lg text-center text-white mx-8">
+        Driven by a passion for technology and a commitment to excellence, I
+        strive to create innovative solutions that positively impact the world,
+        while inviting visitors to explore my portfolio to witness the fusion of
+        creativity and technical expertise.
+      </p>
     </div>
   );
-}
+};
 
 export default About;
