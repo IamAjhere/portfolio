d79c226f40874309f46668c4c4b22fec90ef3612
IamAjhere <Ajcreationsajmal@gmail.com>
2023-04-07 23:08:45 +0530

    update/ui change for about

--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -12,7 +12,7 @@ const About: React.FC<AboutProps> = ({ profile }) => {
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center bg-transparent">
-      <div className="rounded-full overflow-hidden w-32 h-32 mb-8">
+      <div className="rounded-full overflow-hidden w-48 h-48 mb-8">
         <img
           src={profile.avatar_url}
           alt={profile.name}
@@ -20,15 +20,17 @@ const About: React.FC<AboutProps> = ({ profile }) => {
         />
       </div>
       <h1 className="text-6xl font-bold text-center text-white mb-4">
-        Hi, I am {profile.name}
+        Hello! I'm {profile.name}
       </h1>
-      <p className="text-xl text-center text-white mx-8 mb-4">{profile.bio}</p>
-      <p className="text-lg text-center text-white mx-8">
-        Driven by a passion for technology and a commitment to excellence, I
-        strive to create innovative solutions that positively impact the world,
-        while inviting visitors to explore my portfolio to witness the fusion of
-        creativity and technical expertise.
+      <p className="text-xl text-center text-white mx-8 mb-4">
+        Full Stack Engineer & Cloud Computing Enthusiast
       </p>
+      <div className=" bg-white rounded-lg p-10 mx-32 backdrop-blur-md shadow-lg text-lg text-center text-black">
+        A full stack software engineer from Anuradhapura, Sri Lanka, proficient
+        in MERN stack and AWS. Committed to staying up-to-date and ensuring
+        secure, efficient solutions. Discover my work, skills, and achievements
+        on this website. Let's create extraordinary projects together!
+      </div>
     </div>
   );
 };
