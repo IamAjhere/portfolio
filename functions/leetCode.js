199fc1bc867598eff750f7078a997139998a8936
IamAjhere <Ajcreationsajmal@gmail.com>
2023-04-23 11:11:48 +0530

    leet code function cache

--- functions/leetCode.js
+++ functions/leetCode.js
@@ -23,10 +23,26 @@ const LEETCODE_SKILLS_QUERY = `
     }
   }
 `;
-
+// Cache configuration
+const CACHE_DURATION_MS = 5 * 60 * 1000; // 5 minutes
+let cache = {
+  timestamp: null,
+  data: null,
+};
 const handler = async (event, context) => {
   if (event.httpMethod === "GET") {
+    const currentTime = new Date().getTime();
+
+    // Check if the cache is still valid
+    if (cache.timestamp && currentTime - cache.timestamp <= CACHE_DURATION_MS) {
+      console.log("Fetching From Cache");
+      return {
+        statusCode: 200,
+        body: JSON.stringify(cache.data),
+      };
+    }
     try {
+      console.log("Fetching From LeetCode.");
       const response = await axios.post(LEETCODE_SKILLS_GRAPHQL, {
         query: LEETCODE_SKILLS_QUERY,
         variables: { username: LEETCODE_USERNAME },
